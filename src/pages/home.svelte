10b8d336be71e41dbc6f5097d54213e3d8519780
bhatr <bhatr@umich.edu>
2022-03-31 04:02:52 -0400

    added user name into home menu

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,9 +1,11 @@
 <script>
     import MenuItem from "../components/menuItem.svelte";
+    import { apiUser } from "../stores/auth.js";
+    $: console.log($apiUser);
 </script>
 <main class="flex flex-col items-center gap-2 justify-center">
     <img class="w-24" src="/user06a.svg" alt="profile pic">
-    <h2 class="italic font-abeezee text-lg mb-4 focus:outline-none">👋 Welcome, Doae !</h2>
+    <h2 class="italic font-abeezee text-lg mb-4 focus:outline-none">👋 Welcome, {$apiUser.first_name}!</h2>
     <div class="flex flex-wrap w-full p-4 gap-x-16 gap-y-4 justify-center">
         <MenuItem route="dating" name="Speed Dating" />
         <MenuItem route="team" name="Manage Your Team" />
