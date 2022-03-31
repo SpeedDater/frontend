7081d2b9aa45c41837e6e3590b92e458b689c356
bhatr <bhatr@umich.edu>
2022-03-31 04:03:16 -0400

    styled login button correctly

--- src/pages/login.svelte
+++ src/pages/login.svelte
@@ -1,7 +1,14 @@
+<script>
+    import { clientID } from '../stores/auth.js';
+</script>
 <div class="font-roboto text-lg text-navy absolute transform -translate-y-1/2 -translate-x-1/2 left-1/2 top-1/2">
     <img class="mx-auto mb-4" src="/umlogo.svg" alt="University of Michigan Logo">
     <h1 class="text-center text-xl font-semibold focus:outline-none">SpeedDaters</h1>
-    <h2 class="text-center mb-4">Entrepreneurial Creativity (P/ALA 223)</h2>
-    <a class="mx-auto block w-20 bg-navy px-4 py-2 text-white"
-       href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email&response_type=code&include_granted_scope=true&redirect_uri=http://localhost:8080/login/complete/&client_id=370562730007-nsmbvtmu29a0nuutont40b1g9jvgr2m6.apps.googleusercontent.com">Login</a>
+    <h2 class="text-center mb-12">Entrepreneurial Creativity (P/ALA 223)</h2>
+    <a class="mx-auto block w-44 bg-navy px-4 py-4 text-white text-center"
+       href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email&response_type=code&include_granted_scope=true&redirect_uri=http://localhost:8080/login/complete/&client_id={$clientID}">
+       <span>Login with </span> 
+       <img class="inline w-8" src="/google logo.png" alt="Google Logo">
+       
+    </a>
 </div>
