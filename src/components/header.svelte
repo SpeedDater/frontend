14a458c2b2be761d505082b2cf7a56ccb78b7b23
bhatr <bhatr@umich.edu>
2022-03-31 04:00:24 -0400

    added logout button

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,11 +1,14 @@
 
 <script>
-    import { useLocation } from 'svelte-navigator';
+    import { useLocation, Link, link } from 'svelte-navigator';
     const location = useLocation();
 </script>
 {#if $location.pathname != '/login'}
-<header class="ml-10 mt-10 text-navy mb-10">
+<Link to="/">
+<header class="ml-10 mt-10 text-navy mb-10 block">
     <img class="inline-block w-32 mr-2" src="/umlogo.svg" alt="U of M Logo">
     <h1 class="text-2xl inline-block font-adamina">P/ALA 223</h1>
+    <a class="inline-block text-right" use:link href="/logout">Logout</a>
 </header>
+</Link>
 {/if}
