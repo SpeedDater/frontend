1f9b6fa9247dda361698b555863e94e2d79add86
bhatr <bhatr@umich.edu>
2022-03-31 16:45:24 -0400

    inject user content into dating profile

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,12 +1,15 @@
+<script>
+    import { apiUser } from "../stores/auth.js";
+</script>
   <div class="grow font-abeezee p-14 bg-white mt-6 rounded-md text-white mx-4">
     <!-- First Section -->
     <div class="bg-navy hidden sm:block rounded-md h-full">
         <img class="w-24 transform -translate-y-1/4 -translate-x-1/4" src="/user02c.svg" alt="profile pic">
         <!-- name -->
-        <div class="text-white text-2xl font-roboto">Doae Kim</div>
+        <div class="text-white text-2xl font-roboto">{ `${$apiUser.first_name} ${$apiUser.last_name}`}</div>
         <div class="flex flex-row px-2">
             <div class="">
-                <div class="text-white">doaekim@umich.edu</div>
+                <div class="text-white">{$apiUser.email}</div>
                 <div>(123) 456-7890</div>
             </div>
             <a class="self-center mr-0 ml-auto inline-block text-center px-4 py-2 bg-yellowbutton rounded-sm font-roboto italic font-bold" href=".">Send Email</a>
