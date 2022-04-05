8140565eaf07e01cab7a5c6c4baaf57e9ab865c7
bhatr <bhatr@umich.edu>
2022-04-04 20:11:47 -0400

    changed origin so not hard coded

--- src/pages/login.svelte
+++ src/pages/login.svelte
@@ -6,7 +6,7 @@
     <h1 class="text-center text-xl font-semibold focus:outline-none">SpeedDaters</h1>
     <h2 class="text-center mb-12">Entrepreneurial Creativity (P/ALA 223)</h2>
     <a class="mx-auto block w-44 bg-navy px-4 py-4 text-white text-center rounded-md"
-       href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email&response_type=code&include_granted_scope=true&redirect_uri=http://localhost:8080/login/complete/&client_id={$clientID}">
+       href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email&response_type=code&include_granted_scope=true&redirect_uri={window.location.origin}/login/complete/&client_id={$clientID}">
        <span>Login with </span> 
        <img class="inline w-8" src="/google logo.png" alt="Google Logo">
        
