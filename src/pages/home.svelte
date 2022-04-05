a6ca249aaaab9160944e66b71371428eb2bb1f75
bhatr <bhatr@umich.edu>
2022-04-04 20:10:49 -0400

    pfp for home

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -4,7 +4,7 @@
     $: console.log($apiUser);
 </script>
 <main class="flex flex-col items-center gap-2 justify-center">
-    <img class="w-24" src="/user06a.svg" alt="profile pic">
+    <img class="w-24 rounded-full" src="{$apiUser.profile_picture}" alt="profile pic">
     <h2 class="italic font-abeezee text-lg mb-4 focus:outline-none">👋 Welcome, {$apiUser.first_name}!</h2>
     <div class="flex flex-wrap w-full p-4 gap-x-16 gap-y-4 justify-center">
         <MenuItem route="dating" name="Speed Dating" />
